391b08c807d597c958a9f073ba7ad44a615ff9bf
saubhik <saubhik.mukherjee@gmail.com>
2020-04-23 21:24:10 +0900

    add more args for safety

--- script.py
+++ script.py
@@ -61,6 +61,9 @@ def send_email(subscriber: Dict, message: str) -> None:
 
 def run_service():
     options = webdriver.ChromeOptions()
+    options.add_argument("--disable-extensions")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--start-maximized")
     options.add_argument("--no-sandbox")
     options.add_argument("--headless")
     mobile_emulation = {
